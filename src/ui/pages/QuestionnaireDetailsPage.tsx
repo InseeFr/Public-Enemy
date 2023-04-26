ff115cf348360da981794294a1f92cd47dc4a113
David Darras <david.darras@insee.fr>
2023-04-26 17:16:52 +0200

    feat: [#59] add link to pogues from questionnaire details page

--- src/ui/pages/QuestionnaireDetailsPage.tsx
+++ src/ui/pages/QuestionnaireDetailsPage.tsx
@@ -1,8 +1,10 @@
+import LaunchIcon from "@mui/icons-material/Launch";
 import SettingsIcon from "@mui/icons-material/Settings";
 import { Grid, IconButton, Stack, Typography } from "@mui/material";
 import { Questionnaire } from "core/application/model";
 import { useApiMutation } from "core/infrastructure/hooks/useApiMutation";
 import { useApiQuery } from "core/infrastructure/hooks/useApiQuery";
+import { getEnvVar } from "core/utils/env";
 import { memo } from "react";
 import { useIntl } from "react-intl";
 import { Link, useNavigate, useParams } from "react-router-dom";
@@ -20,6 +22,7 @@ export const QuestionnaireDetailsPage = memo(
     const { id } = useParams();
     const intl = useIntl();
     const navigate = useNavigate();
+    const poguesUrl = getEnvVar("VITE_POGUES_URL");
 
     const { isLoading, data: questionnaire } = useApiQuery(
       ["questionnaire", id],
@@ -72,7 +75,18 @@ export const QuestionnaireDetailsPage = memo(
                       : {questionnaire.context.value}
                     </Typography>
                   </Grid>
-
+                  <Grid item xs={12} sm={6}>
+                    <Typography variant="body2" gutterBottom>
+                      <a
+                        href={`${poguesUrl}/questionnaire/${questionnaire.poguesId}`}
+                      >
+                        {intl.formatMessage({
+                          id: "questionnaire_pogues",
+                        })}
+                        <LaunchIcon fontSize="inherit"></LaunchIcon>
+                      </a>
+                    </Typography>
+                  </Grid>
                   <Subtitle>
                     {intl.formatMessage({
                       id: "questionnaire_survey_units",
