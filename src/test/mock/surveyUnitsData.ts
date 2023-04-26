a2f3002e3a5d2e245c7e78308d8c3c8882a0dd04
David Darras <david.darras@insee.fr>
2023-04-26 18:31:43 +0200

    test: fix property values for mocked objects

--- src/test/mock/surveyUnitsData.ts
+++ src/test/mock/surveyUnitsData.ts
@@ -4,20 +4,20 @@ export const surveyUnitsData: SurveyUnitsData = {
   questionnaireModelId: "l8wwljbo",
   surveyUnits: [
     {
-      id: "13-CAPI-EF0001",
-      displayableId: "EF0001",
+      id: "13-CAPI-1",
+      displayableId: 1,
     },
     {
-      id: "13-CAPI-EF0002",
-      displayableId: "EF0002",
+      id: "13-CAPI-2",
+      displayableId: 2,
     },
     {
-      id: "13-CAPI-EF0003",
-      displayableId: "EF0003",
+      id: "13-CAPI-3",
+      displayableId: 3,
     },
     {
-      id: "13-CAPI-EF0004",
-      displayableId: "EF0004",
+      id: "13-CAPI-4",
+      displayableId: 4,
     },
   ],
 };
