abbfae984eb1a4bced91a7cd06821329f2c5d25f
Laurent Caouissin <caouissin.laurent@gmail.com>
2024-01-25 16:32:55 +0100

    chore: remove useConstCallback

--- src/core/infrastructure/QuestionnaireRepository.ts
+++ src/core/infrastructure/QuestionnaireRepository.ts
@@ -2,7 +2,6 @@ import { Questionnaire, SurveyContext } from "core/application/model";
 import { QuestionnaireRepositoryPort } from "core/application/port";
 import { deleteRequest, getRequest } from "core/utils/http";
 import { postRequestMultiPart } from "core/utils/http/fetcher";
-import { useConstCallback } from "./hooks/useConstCallback";
 
 /**
  * Get Questionnaire Repository
@@ -13,81 +12,77 @@ export function createQuestionnaireRepository(
   apiUrl: string,
   token?: string
 ): QuestionnaireRepositoryPort {
-  const getQuestionnaires = useConstCallback((): Promise<Questionnaire[]> => {
+  const getQuestionnaires = (): Promise<Questionnaire[]> => {
     return getRequest<Questionnaire[]>(`${apiUrl}/questionnaires`)(token);
-  });
+  };
 
-  const getQuestionnaire = useConstCallback(
-    (id: number): Promise<Questionnaire> => {
-      return getRequest<Questionnaire>(`${apiUrl}/questionnaires/${id}`)(token);
-    }
-  );
+  const getQuestionnaire = (id: number): Promise<Questionnaire> => {
+    return getRequest<Questionnaire>(`${apiUrl}/questionnaires/${id}`)(token);
+  };
 
   /**
    * get questionnaire filled with pogues informations (not the db one)
    */
-  const getPoguesQuestionnaire = useConstCallback(
-    (poguesId: string): Promise<Questionnaire> => {
-      return getRequest<Questionnaire>(
-        `${apiUrl}/questionnaires/pogues/${poguesId}`
-      )(token);
-    }
-  );
+  const getPoguesQuestionnaire = (poguesId: string): Promise<Questionnaire> => {
+    return getRequest<Questionnaire>(
+      `${apiUrl}/questionnaires/pogues/${poguesId}`
+    )(token);
+  };
 
   /**
    * get questionnaire from pogues id (the db one)
    */
-  const getQuestionnaireFromPoguesId = useConstCallback(
-    (poguesId: string): Promise<Questionnaire> => {
-      return getRequest<Questionnaire>(
-        `${apiUrl}/questionnaires/${poguesId}/db`
-      )(token);
+  const getQuestionnaireFromPoguesId = (
+    poguesId?: string
+  ): Promise<Questionnaire> => {
+    return getRequest<Questionnaire>(`${apiUrl}/questionnaires/${poguesId}/db`)(
+      token
+    );
+  };
+
+  const addQuestionnaire = (
+    questionnaire: Questionnaire
+  ): Promise<Questionnaire> => {
+    const formData = new FormData();
+    const questionnaireRest = {
+      poguesId: questionnaire.poguesId,
+      context: questionnaire.context,
+    };
+    formData.append("questionnaire", JSON.stringify(questionnaireRest));
+
+    if (questionnaire.surveyUnitData) {
+      formData.append("surveyUnitData", questionnaire.surveyUnitData);
     }
-  );
 
-  const addQuestionnaire = useConstCallback(
-    (questionnaire: Questionnaire): Promise<Questionnaire> => {
-      const formData = new FormData();
-      const questionnaireRest = {
-        poguesId: questionnaire.poguesId,
-        context: questionnaire.context,
-      };
-      formData.append("questionnaire", JSON.stringify(questionnaireRest));
+    return postRequestMultiPart<Questionnaire>(
+      `${apiUrl}/questionnaires/add`,
+      formData
+    )(token);
+  };
 
-      if (questionnaire.surveyUnitData) {
-        formData.append("surveyUnitData", questionnaire.surveyUnitData);
-      }
+  const editQuestionnaire = (
+    questionnaire: Questionnaire
+  ): Promise<Questionnaire> => {
+    const formData = new FormData();
+    formData.append("context", JSON.stringify(questionnaire.context));
 
-      return postRequestMultiPart<Questionnaire>(
-        `${apiUrl}/questionnaires/add`,
-        formData
-      )(token);
+    if (questionnaire.surveyUnitData) {
+      formData.append("surveyUnitData", questionnaire.surveyUnitData);
     }
-  );
 
-  const editQuestionnaire = useConstCallback(
-    (questionnaire: Questionnaire): Promise<Questionnaire> => {
-      const formData = new FormData();
-      formData.append("context", JSON.stringify(questionnaire.context));
+    return postRequestMultiPart<Questionnaire>(
+      `${apiUrl}/questionnaires/${questionnaire.id}`,
+      formData
+    )(token);
+  };
 
-      if (questionnaire.surveyUnitData) {
-        formData.append("surveyUnitData", questionnaire.surveyUnitData);
-      }
-
-      return postRequestMultiPart<Questionnaire>(
-        `${apiUrl}/questionnaires/${questionnaire.id}`,
-        formData
-      )(token);
-    }
-  );
-
-  const deleteQuestionnaire = useConstCallback((id: number): Promise<void> => {
+  const deleteQuestionnaire = (id: number): Promise<void> => {
     return deleteRequest<void>(`${apiUrl}/questionnaires/${id}/delete`)(token);
-  });
+  };
 
-  const getSurveyContexts = useConstCallback((): Promise<SurveyContext[]> => {
+  const getSurveyContexts = (): Promise<SurveyContext[]> => {
     return getRequest<SurveyContext[]>(`${apiUrl}/contexts`)(token);
-  });
+  };
 
   return {
     getQuestionnaires,
--- src/core/infrastructure/SurveyUnitRepository.ts
+++ src/core/infrastructure/SurveyUnitRepository.ts
@@ -2,7 +2,6 @@ import { SurveyUnitsData, SurveyUnitsMessages } from "core/application/model";
 import { SurveyUnitRepositoryPort } from "core/application/port";
 import { getRequest } from "core/utils/http";
 import { postRequestMultiPart, putRequest } from "core/utils/http/fetcher";
-import { useConstCallback } from "./hooks/useConstCallback";
 
 /**
  * Get SurveyUnit Repository
@@ -13,38 +12,33 @@ export function createSurveyUnitRepository(
   apiUrl: string,
   token?: string
 ): SurveyUnitRepositoryPort {
-  const getSurveyUnitsData = useConstCallback(
-    (questionnaireId: number, modeName: string): Promise<SurveyUnitsData> => {
-      return getRequest<SurveyUnitsData>(
-        `${apiUrl}/questionnaires/${questionnaireId}/modes/${modeName}/survey-units`
-      )(token);
-    }
-  );
+  const getSurveyUnitsData = (
+    questionnaireId: number,
+    modeName: string
+  ): Promise<SurveyUnitsData> => {
+    return getRequest<SurveyUnitsData>(
+      `${apiUrl}/questionnaires/${questionnaireId}/modes/${modeName}/survey-units`
+    )(token);
+  };
 
-  const checkSurveyUnitsCSV = useConstCallback(
-    (
-      poguesId: string,
-      surveyUnitCSVData: File
-    ): Promise<SurveyUnitsMessages> => {
-      const formData = new FormData();
-      formData.append("surveyUnitData", surveyUnitCSVData);
+  const checkSurveyUnitsCSV = (
+    poguesId: string,
+    surveyUnitCSVData: File
+  ): Promise<SurveyUnitsMessages> => {
+    const formData = new FormData();
+    formData.append("surveyUnitData", surveyUnitCSVData);
 
-      return postRequestMultiPart<SurveyUnitsMessages>(
-        `${apiUrl}/questionnaires/${poguesId}/checkdata`,
-        formData
-      )(token);
-    }
-  );
-
-  const resetSurveyUnit = useConstCallback(
-    (surveyUnitId: string): Promise<void> => {
-      return putRequest<void>(
-        `${apiUrl}/survey-units/${surveyUnitId}/reset`,
-        undefined
-      )(token);
-    }
-  );
-
+    return postRequestMultiPart<SurveyUnitsMessages>(
+      `${apiUrl}/questionnaires/${poguesId}/checkdata`,
+      formData
+    )(token);
+  };
+  const resetSurveyUnit = (surveyUnitId: string): Promise<void> => {
+    return putRequest<void>(
+      `${apiUrl}/survey-units/${surveyUnitId}/reset`,
+      undefined
+    )(token);
+  };
   return {
     getSurveyUnitsData,
     checkSurveyUnitsCSV,
