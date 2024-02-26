ac105b7592d58a675c07c73967f0a46874e07dc6
Laurent Caouissin <caouissin.laurent@gmail.com>
2024-02-26 10:51:54 +0100

    chore: fix reading env variable

--- src/core/utils/configuration/get-configuration.ts
+++
@@ -1,5 +0,0 @@
-const basePath = `${window.location.origin}`;
-
-const getFile = (url: string) => fetch(url).then((r) => r.json());
-
-export const getConfiguration = () => getFile(`${basePath}/configuration.json`);
