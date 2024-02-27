7322976d43a1040fca60cb0773cb28cdcb22566c
Laurent Caouissin <38245508+laurentC35@users.noreply.github.com>
2024-02-27 16:49:36 +0100

    chore: improve env variable (#90)
    * chore: improve env variable
    
    * fix: typo

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,25 @@
 /// <reference types="vite/client" />
-interface ImportMetaEnv extends Readonly<Record<string, string>> {
+
+// When we getEnvVar("XXX"), XXX is typed by these keys
+interface envKeys {
   readonly VITE_API_URL: string;
   readonly VITE_ORCHESTRATOR_URL: string;
+  readonly VITE_POGUES_URL: string;
+  readonly VITE_DOCUMENTATION_URL: string;
   readonly VITE_LOCALE: LocaleType;
+  readonly VITE_AUTH_TYPE: "none" | "oidc";
+  readonly VITE_AUTH_URL: string;
+  readonly VITE_REALM: string;
+  readonly VITE_CLIENT_ID: string;
+  readonly VITE_AUTH_TOKEN_FIELD: oidcTokensFieldString;
 }
+// Overload ImportMetaEnv for vite, when we use import.meta.env.XXX, XXX is typed by envKeys and default vite env variable.
+interface ImportMetaEnv extends Readonly<Record<string, string>>, envKeys {}
 
+// When we use window._env_ in code, you have access to envKeys (autocompletion)
+interface Window {
+  _env_?: ImportMetaEnv;
+}
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
