f77b151cccb41cc56f7a5d4a61142628900ad955
Mailine Nguyen <64129348+MailineN@users.noreply.github.com>
2025-03-04 09:43:02 +0100

    fix: remove not existing env var

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import topLevelAwait from "vite-plugin-top-level-await";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: import.meta.env.VITE_OIDC_ISSUER || "/",
+  base: "/",
   server: {
     port: 3000,
   },
